dbf399b2965ab7c91f6911f319b389ef37126fcb
Nemo505 <dahliahalesia8@gmail.com>
2021-02-27 21:14:46 +0630

    first migrate

---
+++ database/migrations/2021_02_27_143256_create_articles_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn');
+            $table->string('title');
+            $table->string('subcontent');
+            $table->text('image');
+            $table->string('caption');
+            $table->date('publishdate');
+            $table->longtext('content');
+
+            $table->foreignId('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->foreignId('language_id')
+                ->references('id')->on('languages')
+                ->onDelete('cascade');
+            $table->foreignId('category_id')
+                ->references('id')->on('categories')
+                ->onDelete('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
